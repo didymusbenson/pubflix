7be92c1884e5724dff912dc68b69f039100de9e5
Didymus <didymus.benson@gmail.com>
2018-01-17 09:12:47 -0700

    tostring

--- watch/script.js
+++ watch/script.js
@@ -1,5 +1,5 @@
 if (window.location.search.indexOf('movie=') > -1) {
-    var movie = window.location.split("=")[1];
+    var movie = window.location.toString.split("=")[1];
     alert('movie' + movie);
 } else {
     alert('movie not here');
